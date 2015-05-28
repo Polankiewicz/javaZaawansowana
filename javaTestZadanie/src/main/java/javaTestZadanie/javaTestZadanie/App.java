c2ebc0fd55c8793be25e588fdc411714bd6a6b02
Polankiewicz <damianpol2@o2.pl>
2015-05-28 09:25:04 +0200

    project

---
+++ javaTestZadanie/src/main/java/javaTestZadanie/javaTestZadanie/App.java
@@ -0,0 +1,13 @@
+package javaTestZadanie.javaTestZadanie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
